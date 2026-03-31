71fbb495a95a33d46042f5f89b41e6db7cc5acdc
Fractales <nsucucx@muimg.edu.gt>
2026-03-31 11:33:01 -0600

    fix puerto railway

--- server.js
+++ server.js
@@ -251,6 +251,6 @@ app.get("/delete/:id", async (req, res) => {
 // ======================
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Servidor en " + PORT);
 });
