25a636372da06d323b61fc9339d18b7e065cc29d
Fractales <nsucucx@muimg.edu.gt>
2026-04-02 10:00:51 -0600

    fix mysql definitivo

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ try {
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQLPORT,
     ssl: { rejectUnauthorized: false }
   });
