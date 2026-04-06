b47013392dac81a1633301f8255fe1641ddca374
Fractales <nsucucx@muimg.edu.gt>
2026-04-06 09:44:55 -0600

    fix mysql railway

--- server.js
+++ server.js
@@ -13,32 +13,29 @@ app.use(express.static(__dirname));
 
 let db;
 
-try {
-  console.log("Variables:");
-  console.log("HOST:", process.env.MYSQLHOST);
-  console.log("USER:", process.env.MYSQLUSER);
-  console.log("DB:", process.env.MYSQLDATABASE);
-  console.log("PORT:", process.env.MYSQLPORT);
+if (process.env.MYSQL_URL) {
+  console.log("🌍 Usando DB de Railway");
+
+  const url = new URL(process.env.MYSQL_URL);
 
   db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE, // ✅ correcto
-  port: process.env.MYSQLPORT,
-  ssl: { rejectUnauthorized: false }
-});
-
-  db.connect((err) => {
-    if (err) {
-      console.error("❌ ERROR CONECTANDO MYSQL:", err);
-    } else {
-      console.log("✅ MYSQL CONECTADO");
-    }
+    host: url.hostname,
+    port: url.port,
+    user: url.username,
+    password: url.password,
+    database: url.pathname.replace("/", ""),
+    ssl: { rejectUnauthorized: false }
   });
 
-} catch (error) {
-  console.error("❌ ERROR GENERAL MYSQL:", error);
+} else {
+  console.log("💻 Usando DB local");
+
+  db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "fr@ctales",
+    database: "cupones_db"
+  });
 }
 
 // ======================
